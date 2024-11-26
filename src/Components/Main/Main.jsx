9a6e633da0e85a9d2d424bd9e6e32d35c301048a
sahidhyan <sarathy.sahidhyan@gmail.com>
2024-11-26 22:14:20 +0530

    second commit

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -17,7 +17,7 @@ const Main = () => {
             {!showResult
             ?<>
             <div className="greet">
-                <p><span>Hello,Dev</span></p>
+                <p><span>Hello,Sahidhyan</span></p>
                 <p>How can I help you today</p>
             </div>
             <div className="cards">
